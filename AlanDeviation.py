0d54ad16d56b87417cdef226facd540768c35cb0
unknown <jvf.hus@gmail.com>
2019-11-20 15:13:40 +0100

    improvements to q factor calculations

--- AlanDeviation.py
+++ AlanDeviation.py
@@ -13,10 +13,12 @@ class AlanDeviation:
     def __init__(self, title="Alan deviation with varying Tau values (x-axis)"):
         self._title = title
         self.y = []
-        self.tau = np.linspace(0, 100, 1000)
-        # self.tau = np.linspace(0, 5, 10)
-
+        self.tau = np.linspace(0, 100, 100) # self.tau = np.linspace(0, 5, 10)
         self.tau_result = []
+        self.alan_dev_result = []       
+        self.error = []          
+        self.data_points = []         
+        self.avg_adev = 0
 
     def generate_noise(self, N=10000):
         # generate some frequency noise data with N data points.
@@ -27,8 +29,12 @@ class AlanDeviation:
 
     def add_data(self, data):
         self.y = data
+        self.calculate_adev()
 
     def calculate_adev(self, sample_rate=1):
+        # x = allantools.noise.white(10000)
+        # (t2, ad, ade, adn) = allantools.adev(
+        #     x, rate=sample_rate, data_type="freq", taus=self.tau)
         (t2, ad, ade, adn) = allantools.adev(
             self.y, rate=sample_rate, data_type="freq", taus=self.tau)
 
@@ -36,10 +42,22 @@ class AlanDeviation:
         self.alan_dev_result = ad               # Alan deviations per tau
         self.error = ade                        # errors of alan adeviations
         self.data_points = adn                  # values of N for t
+
+        # Determine a tau value
+        tau_index = 40
+        self.avg_adev = ad[tau_index]
+        # print('[TAU {}] = {}'.format(tau_index, round(self.avg_adev,2)))
         return ad
 
+    def reset_data(self):
+        self.y = []
+        self.tau_result = []
+        self.alan_dev_result = []       
+        self.error = []          
+        self.data_points = []         
+        self.avg_adev = 0
+
     def show_plot(self):
-        self.calculate_adev()
         plt.plot(self.tau_result, self.alan_dev_result)  # Plot the results || Alternatively = plt.loglog
         plt.xlabel("Tau (s)")
         plt.ylabel("Alan deviation")
--- index.py
+++ index.py
@@ -8,65 +8,88 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# Setting up the main loop.
+def main():
+    # Setup variable for the seperate tests
+    # Clean data, first 400 is faulty.
+    water_test_19_nov = collect_frequency_data('WaterTest-19-Nov.txt', True)
+    first_test_11_nov = collect_frequency_data('Roomtemp.txt', True)
+
+    # y2 = collect_frequency_data('WaterTest-19-Nov.txt')
+    # plot_data(y2, 'Temperature over Time', 'Temperature (c)')
+
+    # Get q factor with standard deviation. 
+    q1 = q_factor_normal_dist(water_test_19_nov, False)
+    q2 = q_factor_normal_dist(first_test_11_nov, False)
+    # Get q factor with alan deviation. 
+    q1 = q_factor_normal_dist(water_test_19_nov, True)
+    q2 = q_factor_normal_dist(first_test_11_nov, True)
+
+
+
 # Helper functions.
-def get_standard_metrics(data):
+def get_standard_metrics(data, print_metrics=False):
     mu = np.median(data)
     sigma = np.std(data)
     avg = np.average(data)
     n_samples = len(data)
 
-    print('The standard deviation is...', sigma)
-    print('The median is...', mu)
-    print('The average is...', avg)
-    print('The amount of samples is...', n_samples)
+    if print_metrics:
+        print('The standard deviation is...', sigma)
+        print('The median is...', mu)
+        print('The average is...', avg)
+        print('The amount of samples is...', n_samples)
 
     return mu, sigma, n_samples
 
 
-# Setting up the main loop.
-def main():
-    print("\n------------------------WELCOME TO THE ULTIMATE QCM ANALYZER) ----------------------------------\n")
-    print("Today we'll discover which QCM Device will have the most spice!")
-    print("Standard Deviation varies around 4.58")
-    print("Alan Deviation varies between 0.03 and 0.15, depending on the tau")
-    print("\n-----------------------END OF INTRODUCTION----------------------------\n")
-
-    # Read values out of measurement file.
-    data = collect_data()
-    # Setup alan deviation and pass data.
-    a = AlanDeviation()
-    # Alternative, if the changes are small over time, a.logspace_tau()
-    a.add_data(data)
-    # a.logspace_tau()
-    # a.show_plot()
+# Plot data function.
+def plot_data(y_data, title='Frequency over Time', y_label='Frequency (Hz)', x_label='Time (s)'):
+    plt.plot(y_data)
+    plt.ylabel(y_label)
+    plt.xlabel(x_label)
+    plt.title(title)
+    plt.show()
 
 
 # Returns the data of the last experiment (38 000 seconds / samples).
-def collect_data(show_fig=False):
-    _data = pd.read_fwf('./data/Roomtemp.txt', sep=" ",
+def collect_data(data_file_name, show_fig=False):
+    _data = pd.read_fwf('./data/{}'.format(data_file_name), sep=" ",
                         header=None, index=False)
     _data.columns = ["Date", "Time", "Frequency", "Temperature"]
     if show_fig:
-        fig = px.line(_data, x='Time', y='Frequency',
-                      title='Frequency Over Time')
-        fig.show()
+        plot_data(_data, title=data_file_name)
 
     return _data
+
+# Returns the data of the last experiment (38 000 seconds / samples).
+
+
+def collect_temperature_data(data_file_name, show_fig=True):
+    _data = pd.read_fwf('./data/{}'.format(data_file_name), sep=" ",
+                        header=None, index=False)
+    _data.columns = ["Date", "Time", "Frequency", "Temperature"]
+    _temperature_data = _data["Temperature"]
+    if show_fig:
+        plot_data(_temperature_data, 'Time (s)',
+                  'Temperature (c)', 'Temperature over time')
+
+    return _temperature_data
+
 
 # Returns the frequency data only. Data standard value is data of last experiment.
 
 
-def collect_frequency_data(data=collect_data(), show_fig=False, print_metrics=False):
-    data_frequency = data["Frequency"].values
+def collect_frequency_data(data_file_name, show_fig=False, print_metrics=False):
+    data = collect_data(data_file_name)
+    data_frequency = data["Frequency"].values[400:4800]
 
     if print_metrics:
         print('Frequency standard metrics:')
         get_standard_metrics(data_frequency)
 
     if show_fig:
-        fig = px.line(data, x='Time', y='Frequency',
-                      title='Frequency Over Time')
-        fig.show()
+        plot_data(data_frequency, title=data_file_name)
 
     return data_frequency
 
@@ -75,57 +98,110 @@ def collect_frequency_data(data=collect_data(), show_fig=False, print_metrics=Fa
 
 def get_temperature_box_data(show_fig=False, print_metrics=False):
     _data = pd.read_fwf('./data/Arduino control box.txt', sep=" ",
-                        header=None, index=False)
+                        header=None, index=False).head(3000)
     _data.columns = ["Temperature"]
-    data_temperature = _data["Temperature"].values
+    data_temperature = _data["Temperature"]
 
     if print_metrics:
         print('Temperature standard metrics:')
         get_standard_metrics(data_temperature)
 
     if show_fig:
-        plt.plot(data_temperature, title='Temperature over time')
+        plt.plot(data_temperature)
         plt.show()
 
     return data_temperature
 
 
 # Calculates the Q value, based on creating a normal distriubtion(standard deviation, mean) of the 38 000 test.
-def q_factor_normal_dist(data=collect_frequency_data()):
-    (mu, sigma, n_samples) = get_standard_metrics(data)
+def q_factor_normal_dist(data, is_alan_dev=False, show_fig=False):
     # Normal distribution.
+    (mu, sigma, n_samples) = get_standard_metrics(data)
+    # s = np.random.normal(mu, sigma, n_samples)
+
+    if is_alan_dev:
+        # If we want the q factor with the alan deviation, we switch the sigma for the rest of the calcualtion for the alan deviation
+        # Calculate alan deviation to use for calculations
+        a_dev = get_alan_deviation(data)
+        sigma = a_dev
+
+    # Take not the calculated values but the Alan deviation
     s = np.random.normal(mu, sigma, n_samples)
 
     # Convert the normal distribution into a histogram.
     bin_amount = 100
     count, bins, ignored = plt.hist(s, bin_amount, normed=True)
-    # Height of the normal dist@ check the formula for the 
+
+    # Create a plot.
+    if show_fig:
+        # plt.plot(s)
+        plt.xlabel("Frequency buckets")
+        plt.ylabel("Intensity")
+        plt.title("Normal distribution model of Frequency response")
+        plt.show()
+
+    # Height of the normal dist@ check the formula for the
     y = 1/(sigma * np.sqrt(2 * np.pi)) * \
         np.exp(- (bins - mu)**2 / (2 * sigma**2))
     amplitude = max(y)
     y_3db = amplitude/np.sqrt(2)
-    
+
     # In what bucket value falls the 3db cutoff?
     y_sorted = sorted(y)
     y_sorted_index_3db = np.digitize(
         np.array(y_3db), y_sorted, right=True)
     y_3db_bin = y_sorted[y_sorted_index_3db]
 
-    # At what index is the bin value in the unsorted normal distribution? 
+    # At what index is the bin value in the unsorted normal distribution?
     bin_3db_index = np.where(y == y_3db_bin)[0][0]
 
     # What is the frequency (x value) at the 3db cutoff?
     frequency_3db = bins[bin_3db_index]
 
-    # What is the delta frequency? 
+    # What is the delta frequency?
     delta = abs(mu-frequency_3db)
-    q_factor = mu/(2*delta)
-    print('The Q factor for the normal distributino is:', q_factor)
+    q_factor = (mu/(2*delta))
+
+    if is_alan_dev: 
+        print('[Q Factor]: {}K [ADEV]: {}'.format(round(q_factor/1000, 0), round(sigma, 2)))
+    else:
+        print('[Q Factor]: {}K [SDEV]: {}'.format(round(q_factor/1000, 0), round(sigma, 2)))
+
 
     return q_factor
 
 
-# Calculates the Q value, based on creating a normal distriubtion(standard deviation, mean) of the 38 000 test.
-# Run main
+def get_alan_deviation(data, show_fig=False):
+    # Read values out of measurement file.
+    # Setup alan deviation and pass data.
+    a = AlanDeviation()
+    a.reset_data()
+    # Alternative, if the changes are small over time, a.logspace_tau()
+    a.add_data(data)
+    # a.logspace_tau()
+    if show_fig:
+        a.show_plot()
+
+    return a.avg_adev
+
+
+def print_introduction():
+    print("\n------------------------WELCOME TO THE ULTIMATE QCM ANALYZER) ----------------------------------\n")
+    print("Today we'll discover which QCM Device will have the most spice!")
+    print("Standard Deviation varies around 4.58")
+    print("Alan Deviation varies between 0.03 and 0.15, depending on the tau")
+    print("\n-----------------------END OF INTRODUCTION----------------------------\n")
+
+
+# Run the main loop
 main()
-q_factor_normal_dist()
+
+
+'''
+Todos: 
+- Put the helper functions into a seperate file. 
+
+
+
+
+'''
