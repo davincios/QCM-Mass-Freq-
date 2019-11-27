1319af7baacb0db076455c44e3eda4ee004b9dab
unknown <jvf.hus@gmail.com>
2019-11-27 05:40:00 +0100

    saving progress

--- NetworkAnalyzer.py
+++ NetworkAnalyzer.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+
 
 '''
 Network analyzer todos:
@@ -43,12 +45,13 @@ def calculate_z(s11, z0=20):
 # Plot data function.
 
 
-def plot_data(x_data, y_data, title, x_label='Frequency (Hz)', y_label='Impedance (Ohm)'):
+def plot_data(x_data, y_data, title, x_label='Frequency (Hz)', y_label='Conductance (S)'):
     plt.plot(x_data, y_data)
     plt.ylabel(y_label)
     plt.xlabel(x_label)
     plt.title(title)
-    plt.show()
+    # plt.show()
+
 
 # Function to determine q factor
 
@@ -73,45 +76,92 @@ def get_q_factor(g, frequency_array):
 
     # Determine the q factor
     delta = abs(frequency_resonance-frequency_3db)
+    if delta == 0:
+        return 0
     q_factor = frequency_resonance/(2*delta)
     q_factor = round(q_factor, 0)
 
-    # return the q factor. 
+    # return the q factor.
     return q_factor
 
     # print('The index of 3db cutoff is...', index_3db)
     # print('The resonance frequency is...', frequency_resonance)
+    # print('Delta in the q factor calculation is.....', delta)
     # print('The q factor is...', q_factor)
 
 
 # Execute functions.
 
 
-def execute(file_name):
+def execute_data_set(file_name):
     data = collect_data(file_name)
-    frequency_data = data['Frequency']
-    s11_data = data['S11']
+    frequency_data, s11_data = data['Frequency'], data['S11']
 
-    # Create title string for labling.
-    title_string = "Impedance over Frequency - {}".format(file_name)
+    # Calculate Z-Impedance and convert to Conductance (G) and also store in array.
+    g_array = [1/z for z in [calculate_z(s) for s in s11_data]]
+    q_factor = get_q_factor(g_array, frequency_data)
 
-    # Calculate Z / Impedance and store in new array
-    z_array = [calculate_z(s) for s in s11_data]
+    # Create title string to label the resulting calculations.
+    title_string = "Q-factor = {}K on G(S)-f(Hz) with for {}".format(
+        q_factor, file_name)
 
-    # Convert Impedance to Conductance (G) and also store in array.
-    g_array = [1/z for z in z_array]
+    # Error handling. Show graph if error value 0 is returned.
+    if q_factor == 0:
+        q_factor = 'error'
+        result = 'error - {}'.format(file_name)
 
-    get_q_factor(g_array, frequency_data)
-
-    # plot_data(x_data=frequency_data, y_data=g_array, title=title_string,
-    #           x_label='Frequency (Hz)', y_label='Impedance (Ohm)')
+    else:
+        result = round(q_factor)
 
 
-# Execute file functions.
-execute('SETUP5 - D WATER TUBE - M3 M.CSV')
+    
+    # Save the figure
+    plot_data(x_data=frequency_data, y_data=g_array, title="Error {}".format(file_name),
+              x_label='Frequency (Hz)', y_label='Conductance (S)')
+
+
+
+    # Return the results
+    return result
+
+    # print(title_string)
+
+
+# Execute tests.---------------------------
+# execute_data_set('SETUP5 - D WATER TUBE - M3 M.CSV')
+# execute_data_set('SETUP5 - EMPTY TUBE - M2 P.CSV')
+
+def get_file_names():
+    arr = os.listdir('./data/network_analyzer_data')
+    return arr
+
+
+def calculate_and_save():
+    q_results = []
+    file_names = get_file_names()
+    # string_result
+    # remove (phase graphs from filenames)
+    file_names = [x for x in file_names if '(P)' not in x and 'P.CSV' not in x]
+
+    for file_name in file_names:
+        result = execute_data_set(file_name)
+        q_results.append(result)
+        print('FILE NAME: {} QFACTOR: {}'.format(file_name, result))
+
+    # np.savetxt('qfactor_results.csv', file_names, delimiter=',')
+
+
+    # print(q_results)
+# Save the data.
+# np.savetxt('qfactor_results.csv', (col1_array, col2_array, col3_array), delimiter=',')
+calculate_and_save()
 
 
 # def plot_s11():
 #     title_string = "S11 over Frequency - {}".format(file_name)
 #     plot_data(x_data=frequency_data, y_data=s11_data, title=title_string,
 #               x_label='Frequency (Hz)', y_label='S11 (Ohm)')
+
+
+#  plot_data(x_data=frequency_data, y_data=g_array, title=title_string,
+#               x_label='Frequency (Hz)', y_label='Conductance (S)')
